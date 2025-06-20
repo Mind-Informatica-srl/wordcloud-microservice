0973b813f60968cba639221af4d9aafad854b1a7
sicanocchi <sicanocchi@gmail.com>
2025-06-20 09:22:43 +0200

    dev grafici CX

--- src/barre_graph.py
+++ src/barre_graph.py
@@ -103,18 +103,23 @@ def generate_barre_in_pila(cod_domanda, colors, labels, sizes, format, width, he
         fasce_alte = []
 
     # Aggiungere delle linee verticali nere e in grassetto nei due valori delle fasce
-    for i in range(len(fasce_basse)):
-        ax.axvline(fasce_basse[i], color='#fe4254', linestyle='-', linewidth=1.5)
-        ax.text(fasce_basse[i], 1.1, f"{fasce_basse[i]}%", ha='center', va='top', color='#fe4254', fontsize=10, fontweight='bold', transform=ax.get_xaxis_transform(), fontproperties=avenir_font_path)
-
     if cod_domanda != 'C4':
-        for i in range(len(fasce_alte)):
-            ax.axvline(100 - fasce_alte[i], color='#005e34', linestyle='-', linewidth=1.5)
-            ax.text(100 - fasce_alte[i], 1.1, f"{fasce_alte[i]}%", ha='center', va='top', color='#005e34', fontsize=10, fontweight='bold', transform=ax.get_xaxis_transform(), fontproperties=avenir_font_path)
+        for i in range(len(fasce_basse)):
+            ax.axvline(fasce_basse[i], color='#fe4254', linestyle='-', linewidth=1.5)
+            ax.text(fasce_basse[i], 1.1, f"{fasce_basse[i]}%", ha='center', va='top', color='#fe4254', fontsize=10, fontweight='bold', transform=ax.get_xaxis_transform(), fontproperties=avenir_font_path)
     else:
-        for i in range(len(fasce_alte)):
-            ax.axvline(fasce_alte[i], color='#005e34', linestyle='-', linewidth=1.5)
-            ax.text(fasce_alte[i], 1.1, f"{fasce_alte[i]}%", ha='center', va='top', color='#005e34', fontsize=10, fontweight='bold', transform=ax.get_xaxis_transform(), fontproperties=avenir_font_path)
+        for i in range(len(fasce_basse)):
+            ax.axvline(100 - fasce_basse[i], color='#fe4254', linestyle='-', linewidth=1.5)
+            ax.text(100 - fasce_basse[i], 1.1, f"{fasce_basse[i]}%", ha='center', va='top', color='#fe4254', fontsize=10, fontweight='bold', transform=ax.get_xaxis_transform(), fontproperties=avenir_font_path)
+
+    # if cod_domanda != 'C4':
+    for i in range(len(fasce_alte)):
+        ax.axvline(100 - fasce_alte[i], color='#005e34', linestyle='-', linewidth=1.5)
+        ax.text(100 - fasce_alte[i], 1.1, f"{fasce_alte[i]}%", ha='center', va='top', color='#005e34', fontsize=10, fontweight='bold', transform=ax.get_xaxis_transform(), fontproperties=avenir_font_path)
+    # else:
+    #     for i in range(len(fasce_alte)):
+    #         ax.axvline(fasce_alte[i], color='#005e34', linestyle='-', linewidth=1.5)
+    #         ax.text(fasce_alte[i], 1.1, f"{fasce_alte[i]}%", ha='center', va='top', color='#005e34', fontsize=10, fontweight='bold', transform=ax.get_xaxis_transform(), fontproperties=avenir_font_path)
     # Adatta il layout del grafico
     fig.tight_layout()
 
